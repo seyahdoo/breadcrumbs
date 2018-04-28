f3659ec9d3583e98470f07f2b328c0e6ba8af926
Seyyid Ahmed Doğan <contact@seyahdoo.com>
2018-04-28 12:11:35 +0300

    delete restourant seed

--- seed.py
+++ seed.py
@@ -3,26 +3,9 @@
 from sqlalchemy import func
 import sqlalchemy
 
-from model import Restaurant
 from model import connect_to_db, db
 
 from server import app
-
-from yelp_api_call import load_restaurants
-
-
-def set_val_restaurant_id():
-    """Set value for the next restaurant_id after seeding database"""
-
-    # Get the Max restaurant_id in the database
-    result = db.session.query(func.max(Restaurant.restaurant_id)).one()
-    max_id = int(result[0])
-
-    # Set the value for the next restaurant_id to be max_id + 1
-    query = "SELECT setval('restaurants_restaurant_id_seq', :new_id)"
-    db.session.execute(query, {'new_id': max_id + 1})
-    db.session.commit()
-
 
 if __name__ == "__main__":
 
@@ -49,7 +32,3 @@ if __name__ == "__main__":
     db.create_all()
 
     db.session.commit()
-
-        # Import different types of data
-    load_restaurants("Sunnyvale")
-    set_val_restaurant_id()
